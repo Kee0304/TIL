1fe7d5f27b50639249a691311a8f22c752482aa4
기남석 <kee0304@naver.com>
2022-10-13 15:10:36 +0900

    org

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
-from collections import deque
+def permu(idx,r):
+    global perm
+    if idx == r:
+        tmp=[]
+        for n in range(len(bit)):
+            tmp.append(bit[n][1])
+        perm.append(tmp)
+        return
+    else:
+        for i,value in enumerate(arr):
+            if (i,value) not in bit:
+                print(i,value)
+                bit[idx]=(i,value)
+                permu(idx+1,r)
+                bit[idx]=None
 
-print(bool(deque()))
+N=int(input())
+perm=[]
+bit=[0]*N
+arr=[1,2,3,3]
+permu(0,N)
+print(perm)
